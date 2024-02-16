ad5af44bce9959faae6840dcd925a8ab3be221f9
csoeylemez <CSoeylemez@bhakwien10.at>
2024-02-16 11:50:24 +0100

    zufällige zahl von 1 bis 100

--- ZahlenSpiel2/Program.cs
+++ ZahlenSpiel2/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿class Programm
+{
+    static void Main(string[] args)
+    {
+        Random random = new Random();
+        int secretNumber = random.Next(1, 101); // Zufällige Zahl zwischen 1 und 100
+        int attempts = 0;
+    }
+}
